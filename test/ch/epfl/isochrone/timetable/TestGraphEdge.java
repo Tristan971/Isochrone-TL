c7fc8013256782923bae32827ba5a523e0c585fe
Tristan Deloche <tristan.deloche@epfl.ch>
2014-04-07 13:48:52 +0200

    earliest Improvements

--- test/ch/epfl/isochrone/timetable/TestGraphEdge.java
+++ test/ch/epfl/isochrone/timetable/TestGraphEdge.java
@@ -6,8 +6,7 @@ import java.util.HashSet;
 import static org.junit.Assert.*;
 
 /**
- * DEFCLASSE
- *
+ * Classe de test de GraphEdge
  * @author Tristan Deloche (234045)
  */
 public class TestGraphEdge {
@@ -39,28 +38,28 @@ public class TestGraphEdge {
 
     @Test
     public void testEarliestArrivalTime() throws Exception {
-        Stop stop1 = new Stop("stop1", new PointWGS84(0,1));
-        Stop stop2 = new Stop("stop2", new PointWGS84(-1, 0));
-        Stop stop3 = new Stop("stop3", new PointWGS84(-1, 1));
-        Stop stop4 = new Stop("stop4", new PointWGS84(-0.5, 0.5));
-        Stop stop5 = new Stop("stop5", new PointWGS84(0, 0.5));
+        Stop test1 = new Stop("test1", new PointWGS84(1,0.1));
+        Stop test2 = new Stop("test2", new PointWGS84(-1.1, 0));
+        Stop test3 = new Stop("test3", new PointWGS84(-1, 1.1));
+        Stop test4 = new Stop("test4", new PointWGS84(-0.3, 0.1));
+        Stop test5 = new Stop("test5", new PointWGS84(0, 0.8));
 
-        GraphEdge.Builder b1 = new GraphEdge.Builder(stop1);
-        GraphEdge.Builder b2 = new GraphEdge.Builder(stop2);
-        GraphEdge.Builder b3 = new GraphEdge.Builder(stop3);
-        GraphEdge.Builder b4 = new GraphEdge.Builder(stop4);
-        GraphEdge.Builder b5 = new GraphEdge.Builder(stop5);
+        GraphEdge.Builder builder1 = new GraphEdge.Builder(test1);
+        GraphEdge.Builder builder2 = new GraphEdge.Builder(test2);
+        GraphEdge.Builder builder3 = new GraphEdge.Builder(test3);
+        GraphEdge.Builder builder4 = new GraphEdge.Builder(test4);
+        GraphEdge.Builder builder5 = new GraphEdge.Builder(test5);
 
-        b1.addTrip(1002, 2500).addTrip(2000, 2300).addTrip(6000, 6100).setWalkingTime(-1);
-        b2.setWalkingTime(-1);
-        b3.setWalkingTime(1000);
-        b4.addTrip(100, 3000).addTrip(2000, 3500).addTrip(7000, 7500).setWalkingTime(100);
-        b5.addTrip(100, 6000).addTrip(1500, 3500).addTrip(4000, 4500).setWalkingTime(4000);
+        builder1.addTrip(1008, 2800).addTrip(2100, 2340).addTrip(7000, 8000).setWalkingTime(-1);
+        builder2.setWalkingTime(1000);
+        builder3.setWalkingTime(-1);
+        builder4.addTrip(200, 6000).addTrip(1000, 2000).addTrip(6000, 8500).setWalkingTime(200);
+        builder5.addTrip(100, 6000).addTrip(1500, 3500).addTrip(4000, 4500).setWalkingTime(4000);
 
-        assertEquals(2300, b1.build().earliestArrivalTime(1000));
-        assertEquals(SecondsPastMidnight.INFINITE , b2.build().earliestArrivalTime(3000));
-        assertEquals(3000 , b3.build().earliestArrivalTime(2000));
-        assertEquals(1600 , b4.build().earliestArrivalTime(1500));
-        assertEquals(3500 , b5.build().earliestArrivalTime(1250));
+        assertEquals(2340, builder1.build().earliestArrivalTime(1000));
+        assertEquals(3000, builder2.build().earliestArrivalTime(2000));
+        assertEquals(SecondsPastMidnight.INFINITE, builder3.build().earliestArrivalTime(3000));
+        assertEquals(1600, builder4.build().earliestArrivalTime(1400));
+        assertEquals(3500, builder5.build().earliestArrivalTime(1250));
     }
 }
