2a95cabd89856038eb1d8e84d023ae926b52a129
Tristan Deloche <tristan.deloche@epfl.ch>
2014-04-07 13:20:41 +0200

    earliest Improvements

--- test/ch/epfl/isochrone/timetable/TestGraphEdge.java
+++ test/ch/epfl/isochrone/timetable/TestGraphEdge.java
@@ -39,6 +39,28 @@ public class TestGraphEdge {
 
     @Test
     public void testEarliestArrivalTime() throws Exception {
+        Stop stop1 = new Stop("stop1", new PointWGS84(0,1));
+        Stop stop2 = new Stop("stop2", new PointWGS84(-1, 0));
+        Stop stop3 = new Stop("stop3", new PointWGS84(-1, 1));
+        Stop stop4 = new Stop("stop4", new PointWGS84(-0.5, 0.5));
+        Stop stop5 = new Stop("stop5", new PointWGS84(0, 0.5));
 
+        GraphEdge.Builder b1 = new GraphEdge.Builder(stop1);
+        GraphEdge.Builder b2 = new GraphEdge.Builder(stop2);
+        GraphEdge.Builder b3 = new GraphEdge.Builder(stop3);
+        GraphEdge.Builder b4 = new GraphEdge.Builder(stop4);
+        GraphEdge.Builder b5 = new GraphEdge.Builder(stop5);
+
+        b1.addTrip(1002, 2500).addTrip(2000, 2300).addTrip(6000, 6100).setWalkingTime(-1);
+        b2.setWalkingTime(-1);
+        b3.setWalkingTime(1000);
+        b4.addTrip(100, 3000).addTrip(2000, 3500).addTrip(7000, 7500).setWalkingTime(100);
+        b5.addTrip(100, 6000).addTrip(1500, 3500).addTrip(4000, 4500).setWalkingTime(4000);
+
+        assertEquals(2500, b1.build().earliestArrivalTime(1000));
+        assertEquals(SecondsPastMidnight.INFINITE , b2.build().earliestArrivalTime(3000));
+        assertEquals(3000 , b3.build().earliestArrivalTime(2000));
+        assertEquals(1600 , b4.build().earliestArrivalTime(1500));
+        assertEquals(3500 , b5.build().earliestArrivalTime(1250));
     }
 }
