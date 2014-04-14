1ce75993ad34ea19eaba0eea8d277e8662fa4f5b
Tristan Deloche <tristan.deloche@epfl.ch>
2014-04-14 09:10:12 +0200

    EXCEPTIONCEPTION

--- src/ch/epfl/isochrone/tiledmap/OSMTileProvider.java
+++ src/ch/epfl/isochrone/tiledmap/OSMTileProvider.java
@@ -11,9 +11,19 @@ import java.net.URL;
 
 public class OSMTileProvider implements TileProvider {
     @Override
-    public Tile tileAt(int zoom, int x, int y) throws IOException {
+    public Tile tileAt(int zoom, int x, int y) {
         String urlString = "http://a.tile.openstreetmap.org/"+zoom+"/"+x+"/"+y+".png";
-        URL tileURL = new URL(urlString);
-        return new Tile(zoom, x, y, ImageIO.read(tileURL));
+        try {
+            URL tileURL = new URL(urlString);
+            return new Tile(zoom, x, y, ImageIO.read(tileURL));
+        } catch (IOException OSMURLException) {
+            try {
+                OSMURLException.printStackTrace();
+                return new Tile(zoom, x, y, ImageIO.read(getClass().getResource("/images/error-tile.png")));
+            } catch (IOException absentErrorImageException) {
+                absentErrorImageException.printStackTrace();
+            }
+        }
+        throw new UnsupportedOperationException();
     }
 }
--- src/ch/epfl/isochrone/tiledmap/TileProvider.java
+++ src/ch/epfl/isochrone/tiledmap/TileProvider.java
@@ -1,12 +1,10 @@
 package ch.epfl.isochrone.tiledmap;
 
-import java.io.IOException;
-
 /**
  * Interface modélisant un "fournisseur" de tuiles
  * @author Tristan Deloche (234045)
  */
 
 public interface TileProvider {
-    public Tile tileAt(int zoom, int x, int y) throws IOException;
+    public Tile tileAt(int zoom, int x, int y);
 }
