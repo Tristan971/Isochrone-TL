4ace7e4b04ed81c740fc3e6468ceb29bed528199
Tristan Deloche <tristan.deloche@epfl.ch>
2014-05-12 14:26:08 +0200

    very important commit : yolo

--- src/ch/epfl/isochrone/tiledmap/IsochroneTileProvider.java
+++ src/ch/epfl/isochrone/tiledmap/IsochroneTileProvider.java
@@ -4,11 +4,9 @@ import ch.epfl.isochrone.geo.PointOSM;
 import ch.epfl.isochrone.timetable.FastestPathTree;
 import ch.epfl.isochrone.timetable.Stop;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 /**
@@ -38,12 +36,16 @@ public final class IsochroneTileProvider implements TileProvider {
         graphics2D.setColor(colorTable.getColorForIndex(colorTable.getDurations().size()-1));
         graphics2D.fillRect(0, 0, bufferedImage.getWidth(), bufferedImage.getHeight());
 
+        PointOSM point1 = new PointOSM(zoom, x*256, y*256);
+        PointOSM point2 = new PointOSM(zoom, x*256, (y+1)*256);
+        double distancePerPixel = point1.toWGS84().distanceTo(point2.toWGS84()) /256;
+
         for (Integer anInteger : colorTable.getDurations()) {
             graphics2D.setColor(colorTable.getColorForDuration(anInteger));
             for (Stop aStop : fastestPathTree.stops()) {
                 int time = anInteger - ch.epfl.isochrone.math.Math.divF(fastestPathTree.arrivalTime(aStop) - fastestPathTree.startingTime(),60);
                 if (time > 0) {
-                    double param = getRayonAtScale(time*walkingSpeed, zoom);
+                    double param = 60 * time *walkingSpeed / distancePerPixel;
                     double paramx = aStop.position().toOSM(zoom).x()-(x*256);
                     double paramy = aStop.position().toOSM(zoom).y()-(y*256);
                     graphics2D.fill(new Ellipse2D.Double(paramx - param, paramy - param, param*2, param*2));
@@ -51,19 +53,6 @@ public final class IsochroneTileProvider implements TileProvider {
             }
         }
 
-        try {
-            ImageIO.write(bufferedImage, "PNG", new File("test.PNG"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         return new Tile(zoom, x, y, bufferedImage);
-    }
-
-    private double getRayonAtScale(double distanceInMeters, int zoom) {
-        PointOSM pointOSM1 = new PointOSM(zoom, 0, 0);
-        PointOSM pointOSM2 = new PointOSM(zoom, 1, 0);
-        double osmUnitInMeter = pointOSM1.toWGS84().distanceTo(pointOSM2.toWGS84());
-        return Math.round(distanceInMeters / osmUnitInMeter);
     }
 }
